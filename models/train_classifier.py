9b30e8462c942e2b83067b418b714085d2278a80
zidkia <zidkia@gmail.com>
2022-10-14 14:17:28 +0300

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,111 @@
 import sys
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import SVC
+from sklearn.multioutput import MultiOutputClassifier
+
+import pickle
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
+    
+    
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('Data_clean', engine)
+    
+    X = df['message']
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    #print(text)
+    #print(tokens)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    
+    #print(clean_tokens)
+    return clean_tokens
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('clf', RandomForestClassifier())
+    ])
+
+    parameters = {
+    
+         'clf__n_estimators': [5, 10, 20],
+        'clf__min_samples_split': [3, 4]
+    }
+    
+    cv = GridSearchCV (pipeline, param_grid = parameters, n_jobs=-1)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    #converting to a dataframe
+    y_pred = pd.DataFrame (y_pred, columns = y_test.columns)
+    reuslts = (classification_report(y_test.values, y_pred, target_names=y_test.columns.values))
+    print (reuslts)
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open('disaster_model.sav', 'wb'))
 
 
 def main():
