63be18ed8b6588b5b2ba5c44cc29febb31d910d0
zidkia <zidkia@gmail.com>
2022-10-14 14:16:58 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,45 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv("data/disaster_messages.csv")
+    categories = pd.read_csv("data/disaster_categories.csv")
+    return pd.merge(categories, messages, on="id")
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';', expand = True)
+    print (categories)
+    # select the first row of the categories dataframe
+    row = categories.iloc[1,:]
 
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda X: X[:-2])
+    categories.columns = category_colnames
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]   
+    # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+  
+    # drop the original categories column from `df`
+    df.drop(columns=["categories"], inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis=1)
+    
+    df.drop_duplicates(inplace=True)
+    df[df["related"]==2] =1
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('Data_clean', engine, index=False)  
 
 
 def main():
